0f0657375a5d6ffbef0aeec6eeed637c27b16fae
William <william@lhventures.us>
2019-08-02 16:09:50 -0700

    token auth stuff added

--- app/settings.py
+++ app/settings.py
@@ -39,9 +39,18 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'rest_framework',
+    'rest_framework.authtoken',
     'app.pools',
     'app.transactions',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('', include('app.pools.urls')),
     path('', include('app.transactions.urls')),
     path('admin/', admin.site.urls),
+    path('auth-token', views.obtain_auth_token)
 ]
 
 if settings.DEBUG:
